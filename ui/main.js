1bde5448a281506462feb90ab246b7a00c5797b8
Aprocks <aapatel1011.ap@gmail.com>
2017-09-04 20:13:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 // Change the content
-var element= document.getElementById('main-text');
+var element = document.getElementById('main-text');
 element.innerHTML='HI how are you';
 //Change the content on click
-var img=document.getElementById('madi');
-img.onclick=innerHTML='hi how are you';
+var img = document.getElementById('madi');
+img.onclick = innerHTML='hi how are you';
