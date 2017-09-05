e8ee72cadb7720dd385d309de519c1701b8c7aae
Aprocks <aapatel1011.ap@gmail.com>
2017-09-05 13:45:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -82,9 +82,9 @@ function createTemplate(data)
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/:articleName',function(req,res) {
-    var articleName = req.params.articleName;
-     res.send(createTemplate(articles[articleName]));
+app.get('/:artName',function(req,res) {
+    var articleName = req.params.artName;
+     res.send(createTemplate(articles[artName));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
