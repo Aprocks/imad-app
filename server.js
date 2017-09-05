ce51179abbe55c2ffb70edc54e733e30feb77ccb
Aprocks <aapatel1011.ap@gmail.com>
2017-09-05 13:45:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -84,7 +84,7 @@ app.get('/', function (req, res) {
 });
 app.get('/:artName',function(req,res) {
     var articleName = req.params.artName;
-     res.send(createTemplate(articles[artName));
+     res.send(createTemplate(articles[artName]));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
