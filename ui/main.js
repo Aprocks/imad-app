7b4a558c13cd5421e4f5a2feb2ca3081860b551e
Aprocks <aapatel1011.ap@gmail.com>
2017-09-06 14:53:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -31,19 +31,19 @@ button.onclick = function() {
 };*/
 //submit Name
 var nameInput = document.getElementById('name');
-var name = nameInput.value;
+//var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
-submit.onclick = function()
-{
+submit.onclick = function() {
     //Makae a request to the server and send the name
     //Capture the list of names and render them it as list
-    var names = ['name1','name2','name3'];
+    var names = ['name1','name2','name3','name4'];
     var list = '';
-    for(var i=0;i<names.length;i++)
+    for(var i=0; i< names.length; i++)
     {
-        list <- '<li>' +names[i]+ '</li>';
+        list += '<li>' + names[i] + '</li>' ;
     }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
-}
+  var ul = document.getElementById('namelist');
+  
+   ul.innerHTML = list;
+};
 
