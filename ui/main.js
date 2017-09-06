ff6e121a4738197f7d8e21e9d2985207eb7f02fe
Aprocks <aapatel1011.ap@gmail.com>
2017-09-06 15:30:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -29,21 +29,31 @@ button.onclick = function() {
     var span = document.getElementById('count');
     span.innerHTML = counter.toString();
 };*/
-//submit Name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
-var submit = document.getElementById('submit_btn');
+var submit = document.getElementById('submit-btn');
 submit.onclick = function() {
-    //Makae a request to the server and send the name
-    //Capture the list of names and render them it as list
-    var names = ['name1','name2','name3','name4'];
-    var list = '';
-    for(var i=0; i< names.length; i++)
-    {
-        list += '<li>' + names[i] + '</li>' ;
-    }
-  var ul = document.getElementById('namelist');
+    
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function() {
+        if(request.readyState == XMLHttpRequest.DONE) {
+            //Take some action
+            if(request.status == 200) {
+              var names = request,responseText;
+              names = JSON.parse(names);
+              var list = '';
+              for(var i=0; i< names.length; i++)
+              {
+                  list += '<li>' + names[i] + '</li>' ;
+              }
+            var ul = document.getElementById('namelist');
   
-   ul.innerHTML = list;
+             ul.innerHTML = list;
+            }
+        }
+};
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
+    request.open('GET','http://aapatel1011.ap.imad.hasura-app.io/submit-name?name=' +name,true);
+    request.send(null);
 };
 
